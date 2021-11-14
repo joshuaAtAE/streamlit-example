00d366a1341cbf1990385d56e832b2b279c03f05
Joshua Schoenfield <joshua@ae.studio>
2021-11-14 13:06:07 -0300

    Revert formatting, reorder

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,17 +10,18 @@ xanadu_zone = timezone("America/New_York")
 ny_time_zone = timezone("America/New_York")
 
 st.header("Time in Xanadu and New York")
-st.image("Pasted Graphic 14.png", caption=f"Q: What time is it?")
-
+st.image("Pasted Graphic 14.png")
+f"Q: What time is it?"
 st.image("Pasted Graphic 12.png")
 f"A: {datetime.now(ny_time_zone).strftime('%-I:%M')}"
 st.image("Pasted Graphic 14.png")
 f"Q: What time is it in New York?"
 st.image("Pasted Graphic 11.png")
 f"A: Huh?"
-st.image("Pasted Graphic 15.png")
+st.image("Pasted Graphic 14.png")
 f"Q: What time is it in New York?"
 time_in_new_york =  datetime.now(ny_time_zone)
+st.image("Pasted Graphic 15.png")
 f"A: {time_in_new_york.strftime('%-I:%M')}"
 st.image("Pasted Graphic 16.png")
 "Q: At night?"
