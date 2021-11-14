4e2ac533d035195f9baf7f859169176fc7a0aa37
Joshua Schoenfield <joshua@ae.studio>
2021-11-14 12:57:42 -0300

    Add some images

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,9 @@ xanadu_zone = timezone("America/New_York")
 ny_time_zone = timezone("America/New_York")
 
 st.header("Time in Xanadu and New York")
+st.image("Pasted Graphic 14.png")
 f"Q: What time is it?"
+st.image("Pasted Graphic 12.png")
 f"A: {datetime.now(ny_time_zone).strftime('%-I:%M')}"
 f"Q: What time is it in New York?"
 f"A: Huh?"
@@ -19,6 +21,7 @@ time_in_new_york =  datetime.now(ny_time_zone)
 f"A: {time_in_new_york.strftime('%-I:%M')}"
 "Q: At night?"
 hour = int(time_in_new_york.strftime('%H'))
+st.image("Pasted Graphic 17.png")
 if (hour < 4) or (hour > 17):
     "Q: Yes."
 else:
