8994422789e9aab366fdf5a45811b652d1752c37
Joshua Schoenfield <joshua@ae.studio>
2021-11-14 12:51:02 -0300

    Clarify dialogue

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,17 +10,17 @@ xanadu_zone = timezone("America/New_York")
 ny_time_zone = timezone("America/New_York")
 
 st.header("Time in Xanadu and New York")
-f"What time is it?"
-f"{datetime.now(ny_time_zone).strftime('%-I:%M')}"
-f"What time is it in New York?"
-f"Huh?"
-f"What time is it in New York?"
+f"Q: What time is it?"
+f"A: {datetime.now(ny_time_zone).strftime('%-I:%M')}"
+f"Q: What time is it in New York?"
+f"A: Huh?"
+f"Q: What time is it in New York?"
 time_in_new_york =  datetime.now(ny_time_zone)
-f"{time_in_new_york.strftime('%-I:%M')}"
-"At night?"
+f"A: {time_in_new_york.strftime('%-I:%M')}"
+"Q: At night?"
 hour = int(time_in_new_york.strftime('%H'))
 if (hour < 4) or (hour > 17):
-    "Yes."
+    "Q: Yes."
 else:
-    "No."
+    "A: No."
 
