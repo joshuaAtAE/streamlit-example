e767a73b1df01d6746560b745659431d70607e1b
Joshua Schoenfield <joshua@ae.studio>
2021-11-14 12:45:26 -0300

    Add some witty banter

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,16 @@ from pytz import timezone
 ny_time_zone = timezone("America/New_York")
 
 f"What time is it?"
-f"{datetime.now().strftime('%H:%M')}"
+f"{datetime.now().strftime('%h:%M')}"
 f"What time is it in New York?"
 f"Huh?"
 f"What time is it in New York?"
-f"{datetime.now(ny_time_zone).strftime('%H:%M')}"
+time_in_new_york =  datetime.now(ny_time_zone)
+f"{time_in_new_york.strftime('%h:%M')}"
+"At night?"
+hour = int(time_in_new_york.strftime('%H'))
+if (hour < 4) or (hour > 17):
+    "Yes."
+else:
+    "No."
 
